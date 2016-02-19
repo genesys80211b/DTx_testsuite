143acc5d51a0d5ac30c298914fee43ed2c4b5bde
Eric Doyle <doyle.e@husky.neu.edu>
2016-02-19 12:11:04 -0500

    cleaned up folders/files

--- DTx/getipa.m
+++ DTx/getipa.m
@@ -5,17 +5,7 @@ if (r)
     if (ispc), hst=getenv('COMPUTERNAME');
     else hst=getenv('HOSTNAME'); end
 end % END IF ~R
-% if strncmpi(hst,'bdroz-OptiPlex',14)
-%     ipa = uint8(103);
-% elseif strncmpi(hst,'bdroz-Precision',15)
-%     ipa = uint8(202);
-% elseif strncmpi(hst,'bdroz-Vostro',12)
-%     ipa = uint8(102);
-% elseif strncmpi(hst,'curie',5)
-%     ipa = uint8(202);
-% else
-    f = findsdru;
-    ipa = uint8(str2double(strrep(f.IPAddress(end-4:end),'.','')));
-% end % END IF STRCMPI(hst,'')
+f = findsdru;
+ipa = uint8(str2double(strrep(f.IPAddress(end-4:end),'.','')));
 return;
 end % END FUNCTION GETIPA
